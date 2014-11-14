7fb664a6fd8c8008682d8cbcb85aad824a4680ee
Hiroshi Yamamoto <higon@freepress.jp>
2014-11-14 11:45:54 +0900

    Synced `templates/web/index.dart`

--- lib/templates/web/index.dart
+++ lib/templates/web/index.dart
@@ -1,27 +1,18 @@
 import 'package:wsk/wsk.dart';
 import 'package:wsk/routing_utils.dart';
 
-/// Application's main method.
+/// Application's main method
 void main() {
 
-  //
-  // Menu Listeners
-  //
+  // Listeners
   initWebStarterKit();
 
-  //
   // Routing
-  //
-
-  // A URL-to-ElementID table
-  Map<String, String> url_to_id_map = {
-    "/welcome": "#welcome",
+  var url_to_id_map = {
+    "/welcome": "#welcome", // {URL}, {HTML tag id}
     "/home": "#home",
     "/about": "#about",
   };
   newRouter(url_to_id_map).listen();
 
-  // Hide everything but #welcome.
-  // switchContentByUrl(url_to_id_map, "/welcome");
 }
-
